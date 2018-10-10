065a742e3f4045b1eb480e40ea44d1ec3de64f1e
Laurent Michel <laurent1984@gmail.com>
2018-10-10 12:26:19 +0200

    * improve scripts

--- consumer-frontend/scripts/DownloadStaticAssets.sh
+++ consumer-frontend/scripts/DownloadStaticAssets.sh
@@ -1,10 +1,21 @@
 #! /bin/bash
 
+if [ $# -ne 3 ] ; then 
+  echo "Usage: $0 repository user password" 
+  exit
+fi
+
+REPO=$1
+USER=$2
+PWD=$3
+
+REPO=https://$USER:$PWD@${REPO#https://}
+
 cd /data
 mkdir assets
 cd assets
 git init
-git remote add origin https://softozor-deployer:s0ft050r@bitbucket.org/softozor/shopozor-consumer-frontend
+git remote add origin $REPO
 git config core.sparsecheckout true
 echo "src/themes/default/assets" >> .git/info/sparse-checkout
 git pull --depth=1 origin master
--- consumer-frontend/scripts/StartApp.sh
+++ consumer-frontend/scripts/StartApp.sh
@@ -1,6 +1,14 @@
 #! /bin/bash
 
-cd /home/jelastic/ROOT
+if [ $# -ne 1 ] ; then
+  echo "Usage: $0 path-to-context"
+  echo "Example: $0 /home/jelastic/ROOT"
+  exit
+fi
+
+PATH_TO_CONTEXT=$1
+
+cd $PATH_TO_CONTEXT
 yarn
 yarn build
 yarn start
