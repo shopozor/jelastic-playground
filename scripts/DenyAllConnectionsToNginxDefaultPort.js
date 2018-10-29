d9e6325b42f423602d2158854806a0affbdfa8af
Laurent Michel <laurent1984@gmail.com>
2018-10-29 12:26:23 +0100

    fix script

--- scripts/DenyAllConnectionsToNginxDefaultPort.js
+++ scripts/DenyAllConnectionsToNginxDefaultPort.js
@@ -1,3 +1,29 @@
 var APPID = getParam("TARGET_APPID");
 var resp = jelastic.environment.security.GetRules(APPID, session);
-return resp;
+
+return resp.rules.find(function(element) {
+  return element.ports == 80;
+});
+
+/*
+The answer is of type:
+{
+  "result": 0,
+  "className": "com.hivext.api.system.persistence.FirewallRule",
+  "rules": [
+    {
+      "protocol": "ALL",
+      "dst": "ALL",
+      "src": "",
+      "isEnabled": true,
+      "name": "Allow Platform Infrastructure",
+      "action": "ALLOW",
+      "id": 0,
+      "ports": "",
+      "priority": 1,
+      "type": "CUSTOM"
+    },
+    ...
+  ]
+}
+*/
