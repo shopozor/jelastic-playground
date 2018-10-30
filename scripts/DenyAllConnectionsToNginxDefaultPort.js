ccb9e51686e6677ad55f9dbc7d5db59142025a38
Laurent Michel <laurent1984@gmail.com>
2018-10-30 06:16:37 +0100

    test script

--- scripts/DenyAllConnectionsToNginxDefaultPort.js
+++ scripts/DenyAllConnectionsToNginxDefaultPort.js
@@ -1,32 +1,52 @@
-var APPID = getParam("TARGET_APPID");
-var resp = jelastic.environment.security.GetRules(APPID, session);
+var SUCCESS_RESPONSE = { result: 0 };
 
-return JSON.stringify(jelastic);
-
-// this doesn't work:
-// return resp.rules.find(function(element) {
-//   return element.ports == 80;
-// });
-
-/*
-The answer is of type:
-{
-  "result": 0,
-  "className": "com.hivext.api.system.persistence.FirewallRule",
-  "rules": [
-    {
-      "protocol": "ALL",
-      "dst": "ALL",
-      "src": "",
-      "isEnabled": true,
-      "name": "Allow Platform Infrastructure",
-      "action": "ALLOW",
-      "id": 0,
-      "ports": "",
-      "priority": 1,
-      "type": "CUSTOM"
-    },
-    ...
-  ]
+function checkJelasticResponse(response, errorMsg) {
+  if (!response || response.result !== 0) {
+    throw errorMsg + response;
+  }
 }
-*/
+
+function getFirewallRules() {
+  var APPID = getParam("TARGET_APPID");
+  var resp = jelastic.environment.security.GetRules(
+    APPID,
+    session,
+    "bl",
+    "INPUT"
+  );
+  checkJelasticResponse(
+    resp,
+    "Cannot get firewall rules with APPID <" +
+      APPID +
+      ">, session <" +
+      session +
+      ">, nodeGroup <bl>, and direction <INPUT>"
+  );
+  return resp.rules;
+}
+
+function denyAllConnectionsToNginxDefaultPort() {
+  var APPID = getParam("TARGET_APPID");
+  var rules = getFirewallRules();
+  for (var key in rules) {
+    var rule = rules[key];
+    if (rule.ports == 80) {
+      rule.name = "Allow HTTP";
+      rule.action = "ALLOW";
+      var resp = jelastic.environment.security.EditRule(APPID, session, rule);
+      checkJelasticResponse(
+        resp,
+        "Cannot edit firewall rule with APPID <" +
+          APPID +
+          ">, session <" +
+          session +
+          "> and rule <" +
+          rule +
+          ">"
+      );
+      return SUCCESS_RESPONSE;
+    }
+  }
+}
+
+return denyAllConnectionsToNginxDefaultPort();
