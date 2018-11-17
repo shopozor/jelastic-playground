ee780047ec339326e3dce4bb003ca94c80995608
Laurent Michel <laurent1984@gmail.com>
2018-11-17 15:59:48 +0100

    * write script allowing frontends to access api * clean up code

--- apis/scripts/OpenToEnvironment.js
+++ apis/scripts/OpenToEnvironment.js
@@ -4,18 +4,17 @@ function checkJelasticResponse(response, errorMsg) {
   }
 }
 
-function addFirewallRule(rule) {
-  const APPID = getParam("TARGET_APPID");
+function addFirewallRule(envName, rule) {
   const resp = jelastic.environment.security.AddRule(
-    APPID,
+    envName,
     session,
     rule,
     "bl"
   );
   checkJelasticResponse(
     resp,
-    "Cannot add firewall rule with APPID <" +
-      APPID +
+    "Cannot add firewall rule with envName <" +
+      envName +
       ">, session <" +
       session +
       ">, nodeGroup bl, and rule <" +
@@ -24,21 +23,50 @@ function addFirewallRule(rule) {
   );
 }
 
-function allowConnectionFromEnv(port, srcEnv) {
+function getNodesInfo(envName) {
+  const resp = jelastic.environment.control.GetEnvInfo(envName, session);
+  checkJelasticResponse(
+    resp,
+    "Cannot get environment info of environment <" +
+      envName +
+      ">, session <" +
+      session +
+      ">"
+  );
+  return resp.nodes;
+}
+
+function getListOfNodejsNodeIPs(envName) {
+  var result = [];
+  const nodes = getNodesInfo(envName);
+  for (var i = 0; i < nodes.length; ++i) {
+    var node = nodes[i];
+    if (node.nodeType == "nodejs") {
+      result.push(node.intIP);
+    }
+  }
+  return result.toString();
+}
+
+function allowConnectionFromEnv(port, envToOpen, envToAllow) {
   const SUCCESS_RESPONSE = { result: 0 };
   const rule = {
     protocol: "ALL",
-    src: "ENVIRONMENT NODES",
+    src: getListOfNodejsNodeIPs(envToAllow),
     isEnabled: true,
     name: "Allow internal connection",
     action: "ALLOW",
     ports: port,
     type: "CUSTOM",
     direction: "INPUT",
-    relatedEnvName: srcEnv
+    relatedEnvName: envToOpen
   };
-  addFirewallRule(rule);
+  addFirewallRule(envToOpen, rule);
   return SUCCESS_RESPONSE;
 }
 
-return allowConnectionFromEnv(getParam("port"), getParam("srcEnv"));
+return allowConnectionFromEnv(
+  getParam("port"),
+  getParam("envToOpen"),
+  getParam("envToAllow")
+);
