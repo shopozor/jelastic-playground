063038e6a44e52ffcc112a635603c1e7178a07c4
Laurent Michel <laurent1984@gmail.com>
2018-11-28 17:13:55 +0100

    * set two cp nodes * confirm necessity of having $HOME/ROOT added to the pythonpath

--- apis/saleor-api/config/wsgi.py
+++ apis/saleor-api/config/wsgi.py
@@ -7,8 +7,7 @@ try:
 except IOError:
   sys.stderr.write("Couldn't read file: ", activationScriptFilename)
 
-# TODO: is this line really necessary?
-# sys.path.append(os.path.join(os.environ['HOME'], 'ROOT'))
+sys.path.append(os.path.join(os.environ['HOME'], 'ROOT'))
 os.environ['SECRET_KEY'] = 'SECRET_KEY_PLACEHOLDER'
 os.environ['ALLOWED_HOSTS'] = 'ALLOWED_HOSTS_PLACEHOLDER'
 os.environ['DATABASE_URL'] = 'DATABASE_URL_PLACEHOLDER'
