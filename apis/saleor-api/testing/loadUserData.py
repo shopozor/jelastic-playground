2e951d64e2e349005e98404e92ca3dcc6fb5d056
Laurent Michel <laurent1984@gmail.com>
2018-12-11 09:41:43 +0100

    * change 'users' to 'producers' label

--- apis/saleor-api/testing/loadUserData.py
+++ apis/saleor-api/testing/loadUserData.py
@@ -2,9 +2,9 @@ import json
 from saleor.account.models import User
 
 with open('user-data.json') as file:
-    users = json.load(file)['users']
+    producers = json.load(file)['producers']
 
-for user in users:
-    u = User.objects.create(email=user['email'])
-    u.set_password(user['password'])
-    u.save()
+for producer in producers:
+    user = User.objects.create(email=producer['email'])
+    user.set_password(producer['password'])
+    user.save()
